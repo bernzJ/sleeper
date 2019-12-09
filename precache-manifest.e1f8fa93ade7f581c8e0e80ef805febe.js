c133f8b11374b8fb3c4f794bb6a9be4bc6fe92d4
Ben J <ben-0_0-@hotmail.com>
2019-12-08 19:50:26 -0500

    Updates

---
+++ precache-manifest.e1f8fa93ade7f581c8e0e80ef805febe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce8b660307ecce21f1c4781e9ba971a",
+    "url": "/sleeper/index.html"
+  },
+  {
+    "revision": "499f1c82e48ea7f05bec",
+    "url": "/sleeper/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c1df11b25dcc33ead74a",
+    "url": "/sleeper/static/css/main.2b068952.chunk.css"
+  },
+  {
+    "revision": "499f1c82e48ea7f05bec",
+    "url": "/sleeper/static/js/2.d1e30466.chunk.js"
+  },
+  {
+    "revision": "c1df11b25dcc33ead74a",
+    "url": "/sleeper/static/js/main.25986db1.chunk.js"
+  },
+  {
+    "revision": "8217d5a372d0cabeccb4",
+    "url": "/sleeper/static/js/runtime-main.f37213b1.js"
+  },
+  {
+    "revision": "49803d27a5ea523d2aedde4aa1a9df58",
+    "url": "/sleeper/static/media/header.49803d27.jpg"
+  },
+  {
+    "revision": "0176317efcb2f259a7ce2d9d4f46b69f",
+    "url": "/sleeper/static/media/mattress1.0176317e.png"
+  },
+  {
+    "revision": "6468cdfedbb338ec9c01541670584dfd",
+    "url": "/sleeper/static/media/mattress2.6468cdfe.png"
+  },
+  {
+    "revision": "edaa711bde3bf9bb6ae52ed3f9204d6b",
+    "url": "/sleeper/static/media/mattress3.edaa711b.png"
+  }
+]);
